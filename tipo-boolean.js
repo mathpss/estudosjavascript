b9f6441f2d6e20e0b914c9a4a507c9219079ec1f
mathpss <101290895+mathpss@users.noreply.github.com>
2022-03-15 21:37:00 -0300

    Add files via upload

---
+++ tipo-boolean.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const primeironumero = 5;
+const segundonumero = 5;
+let cadastro = false;
+
+
+console.log(primeironumero === segundonumero);
+
+
---
+++ type-string.js
@@ -0,0 +1,10 @@
+
+const texto1 = "Olá mundo ";
+const texto2 = 'Olá, mundo ';
+
+const citacao = 'meu nome é ';
+const meunome = "matheus";
+
+
+
+console.log (texto1 + citacao + meunome)
